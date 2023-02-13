01ba6bfe92b17b472d2fc4b578971128c630ac5a
LiliaSlabka <105985427+LiliaSlabka@users.noreply.github.com>
2023-02-13 14:24:46 +0000

    Deploying to gh-pages from @ LiliaSlabka/goit-js-homework-09@aaac7a01ca484247227e87ed76752081df3d4489 🚀

---
+++ 01-color-switcher.2375ad47.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){console.log("START"),n=setInterval((function(){return document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){console.log("STOP"),clearInterval(n),e.disabled=!0,t.disabled=!1}));var n=null}();
+//# sourceMappingURL=01-color-switcher.2375ad47.js.map
---
+++ 01-color-switcher.57840a49.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){console.log("START"),o=setInterval((()=>document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){console.log("STOP"),clearInterval(o),e.disabled=!0,t.disabled=!1}));let o=null;
+//# sourceMappingURL=01-color-switcher.57840a49.js.map
