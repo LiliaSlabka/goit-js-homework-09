aaac7a01ca484247227e87ed76752081df3d4489
LiliaSlabka <liaslabka@gmail.com>
2023-02-13 16:24:21 +0200

    01-task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
 
+const startBtn = document.querySelector('button[data-start]')
+const stopBtn = document.querySelector('button[data-stop]');
+
+startBtn.addEventListener('click', startColorSwitcher);
+stopBtn.addEventListener('click', stopColorSwitcher);
+
+let intervalId = null;
+
+function startColorSwitcher() {
+    console.log('START')
+    intervalId = setInterval(() => document.body.style.backgroundColor = getRandomHexColor(), 1000);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+}
+
+function stopColorSwitcher() {
+    console.log('STOP')
+    clearInterval(intervalId);
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
